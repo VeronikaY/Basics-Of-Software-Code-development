f7dd1920941c3e8eb90821ab9ef117be7035e802
Veronika <53329908+VeronikaY@users.noreply.github.com>
2020-02-02 20:02:22 +0300

    Update TaskThree.java

--- src/sample/TaskThree.java
+++ src/sample/TaskThree.java
@@ -3,7 +3,6 @@ package sample;
 public class TaskThree {
 public static void main(String[] args) {
     int x = 1, y = 2;
-    System.out.println(String.format("%.7g%n",((Math.sin(x) + Math.cos(y)) / (Math.cos(x) - Math.sin(y))) *
-        Math.tan(x * y)));
+    System.out.println(String.format("%.7g%n",((Math.sin(x)+Math.cos(y))/(Math.cos(x)-Math.sin(y)))*Math.tan(x*y)));
   }
 }
