79c0554a409dbb4a1e9055815fbafe5728cd2fa0
VeronikaY <Verona.7775@mail.ru>
2020-02-02 16:56:09 +0300

    Initial commit

---
+++ src/sample/Function.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Function {
+   public static void main (String[]args){
+       int a=1,b=4,c=5,z=6,res;
+       res=z-((a-3)*b/2)+c;
+       System.out.println(res);
+   }
+}
